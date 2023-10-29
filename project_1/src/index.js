579d70b7dfb4de323896499099c5fcfe8fd92b65
nestorrig <neriga10@hotmail.com>
2023-10-29 15:42:42 -0600

    Clase 13: Proyecto 1; Enriqueciendo la información

--- project_1/src/index.js
+++ project_1/src/index.js
@@ -1,8 +1,7 @@
-console.log('hola page');
+const url = 'https://platzi-avo.vercel.app'
+const appNode = document.querySelector('#app')
 
-const url = 'https://platzi-avo.vercel.app/api/avo'
-
-window.fetch(url)
+window.fetch(`${url}/api/avo`)
     .then((respuesta) => respuesta.json())
 
     .then((respuestaJson) => {
@@ -15,16 +14,52 @@ window.fetch(url)
             console.log(item.name);
 
             const image = document.createElement('img')
-            
-            const name = document.createElement('h2')
-            
-            const price = document.createElement('p')
-            
-            const container = document.createElement('div')
+            image.src = `${url}${item.image}`
 
+            const name = document.createElement('h2')
+            name.textContent = item.name
+
+            const price = document.createElement('p')
+            price.textContent = `$${item.price}`
+
+            const container = document.createElement('div')
             container.append(image, name, price)
+
             allItems.push(container)
 
         })
-        document.body.append(...allItems)
+        appNode.append(...allItems)
     })
+
+/** CON ASYNC y AWAIT
+
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+//web api
+async function fetchData() {
+    const response = await fetch(url),
+    data = await response.json(),
+    allItems = [];
+
+    data.data.forEach((item) => {
+        // create image
+        const image = document.createElement("img");
+        // create title
+        const title = document.createElement("h2");
+        // create price
+        const price = document.createElement("div");
+
+        const container = document.createElement("div");
+        container.append(image, title, price);
+
+        allItems.push(container);
+    });
+
+    document.body.append(...allItems)
+}
+
+fetchData();
+
+
+
+ */
