930404bfe6cb53fb9f915ec2b6170948f18b259f
nestorrig <neriga10@hotmail.com>
2023-11-02 22:09:55 -0600

    Clase 21: Creando las imagenes con JavaScript, modo aleatorio

--- project_2/index.js
+++ project_2/index.js
@@ -1,18 +1,14 @@
 const imagesContainer = document.querySelector('#Container')
 
-let fox = 0
-
-
+const min = 1
+const max = 123
+const fox = () => Math.floor(Math.random() * (max - min) + min)
 
 
 function addNewFox() {
-    fox++
-    if (fox > 123) {
-        return
-    }
     const img = document.createElement('img')
-    img.src = `https://randomfox.ca/images/${fox}.jpg`
-
+    img.src = `https://randomfox.ca/images/${fox()}.jpg`
+    console.log(img.src);
     imagesContainer.appendChild(img)
 }
 
