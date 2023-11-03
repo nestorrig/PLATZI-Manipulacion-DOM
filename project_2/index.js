e4d5431d554822ab7d4c63c62039cc2f7f8098fe
nestorrig <neriga10@hotmail.com>
2023-11-03 00:06:55 -0600

    Clase 24: Comparte el resultado, retos

--- project_2/index.js
+++ project_2/index.js
@@ -1,18 +1,39 @@
-// import { registerImage } from "./lazy.js";
-
+import { registerImage } from "./lazy.js";
+import { foxReady } from "./lazy.js";
+import { restarValue } from "./lazy.js";
 const imagesContainer = document.querySelector('#Container')
+
 
 const min = 1
 const max = 123
 const fox = () => Math.floor(Math.random() * (max - min) + min)
 
-document.querySelector('button').addEventListener('click', () => {
+export let foxWaiting = 0
+
+
+document.querySelector('#Add').addEventListener('click', () => {
     const img = document.createElement('img')
-    img.src = `https://randomfox.ca/images/${fox()}.jpg`
-    // img.dataset.src = `https://randomfox.ca/images/${fox()}.jpg`
-    img.loading = 'lazy'
-    imagesContainer.appendChild(img)
+    // img.src = `https://randomfox.ca/images/${fox()}.jpg`
+    // img.loading = 'lazy'
+    img.dataset.src = `https://randomfox.ca/images/${fox()}.jpg`
+    foxWaiting++
+
+    const div = document.createElement('div')
+    div.className ='img-pic'
+    div.append(img)
+
+    
+    imagesContainer.appendChild(div)
     // añadimos la imagen al registro
-    // registerImage(img)
-    console.log(img.src);
+    registerImage(div)
+})
+document.querySelector('#Delete').addEventListener('click', () => {
+    foxWaiting = 0
+    restarValue()
+
+    imagesContainer.innerHTML = ''
+    console.log('---------------------------------------------------');
+    console.log(`+ ahora hay ${foxWaiting} zorros esperando por cargar`);
+    console.log(`* ahora hay ${foxReady} zorros cargados`);
+
 })
--- project_2/lazy.js
+++ project_2/lazy.js
@@ -1,13 +1,26 @@
+import { foxWaiting } from "./index.js"
+
+
 const isIntersecting = entry => {
     return entry.isIntersecting
 }
+export let foxReady = 0
+
 const loadImage = (entry) => {
-    const image = entry.target
+    const container = entry.target
+    const image = container.firstChild
     const url = image.dataset.src
-    console.log(url);
     image.src = url
+    foxReady++
+
+    console.log('---------------------------------------------------');
+    console.log(`+ ahora hay ${foxWaiting} zorros esperando por cargar`);
+    console.log(`* ahora hay ${foxReady} zorros cargados`);
+
+
+    // console.log(`ahora hay ${foxReady} zorros cargados`);
     // deja de ver el image una ves que ya fue registrado
-    observer.unobserve(image)
+    observer.unobserve(container)
 }
 const observer = new IntersectionObserver( entries => {
     entries
@@ -16,5 +29,13 @@ const observer = new IntersectionObserver( entries => {
 })
 export const registerImage = image => {
     observer.observe(image)
+    console.log('---------------------------------------------------');
+    console.log(`+ ahora hay ${foxWaiting} zorros esperando por cargar`);
+    console.log(`* ahora hay ${foxReady} zorros cargados`);
 }
 //menor codigo en: https://platzi.com/comentario/2524515/
+
+
+export const restarValue = () => {
+    foxReady = 0
+}
